ddf1e7a260a7a5d838b1b53dcc51f258aaf5941f
Syntaxious <SynPlaysMC@gmail.com>
2016-04-10 17:22:31 +0100

    Update Main.php

--- src/GamemodeOP/Main.php
+++ src/GamemodeOP/Main.php
@@ -1,1 +1,29 @@
+<?php
 
+namespace GamemodeOP;
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Server;
+
+class Main extends PluginBase implements Listener{
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("GamemodeOP enabled");
+    }
+    
+    public function onJoinEvent(PlayerJoinEvent $event){
+        $player = $event->getPlayer();
+        if($player->isOp()){
+            return true;
+        }else{
+            $player->setGamemode(0);
+            $player->sendTip("You are in Gamemode 0.")
+        }
+    }else{
+        $player->setGamemode(1)
+}
+}
